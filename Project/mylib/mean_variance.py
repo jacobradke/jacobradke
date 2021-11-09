2cce0293ad0a22daf2d6537bb73bad330b33b3ff
jacobradke <radke.jacob.b@gmail.com>
2021-11-09 09:43:52 -0600

    update

--- Project/mylib/mean_variance.py
+++ Project/mylib/mean_variance.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import datetime 
 import statsmodels.api as sm
 end = datetime.datetime.today()
+start = datetime.datetime(2011,1,1)
 
-def mean_variance(ticker_lst, num_ports, start, end):
+def mean_variance(ticker_lst, num_ports, start, end, benchmark, rf):
     ticker_dict = {}
     for ticker in ticker_lst:
         ticker_dict[ticker] = web.DataReader(ticker, "yahoo", start, end)["Adj Close"]
@@ -18,7 +19,7 @@ def mean_variance(ticker_lst, num_ports, start, end):
     port_weights = []
     num_assets = len(df.columns)
     num_portfolios = num_ports
-    individual_rets = df.resample("Y").last().pct_change().mean()
+    individual_rets = df.resample("A").last().pct_change().mean()
     for port in range(num_portfolios):
         weights = np.random.random(num_assets)
         weights = weights/np.sum(weights)
@@ -33,7 +34,7 @@ def mean_variance(ticker_lst, num_ports, start, end):
     for counter, symbol in enumerate(df.columns.to_list()):
         data[symbol+" Weight"]=[w[counter] for w in port_weights]
     portfolios = pd.DataFrame(data)
-    pct_change["Market"] = web.DataReader("^GSPC", "yahoo", start = start, end = end)["Adj Close"].pct_change()*100
+    pct_change["Market"] = web.DataReader(benchmark, "yahoo", start = start, end = end)["Adj Close"].pct_change()*100
     pct_change = pct_change.dropna()
     beta = {}
     for key in pct_change:
@@ -55,9 +56,17 @@ def mean_variance(ticker_lst, num_ports, start, end):
     col_name = "Portfolio Beta"
     third_col = portfolios.pop(col_name)
     portfolios.insert(2, col_name, third_col)
+    market = web.DataReader(benchmark, "yahoo", start = start, end = end)["Adj Close"]
+    market = pd.DataFrame(market)
+    market_mean = market.resample("M").last().pct_change().mean()*100
+    jensens_alpha = portfolios["Returns"]-(rf + portfolios["Portfolio Beta"]*(market_mean["Adj Close"]-rf))
+    portfolios["Jensen's Alpha"] = jensens_alpha
+    jen_col = "Jensen's Alpha"
+    fourth_col = portfolios.pop(jen_col)
+    portfolios.insert(3, jen_col, fourth_col)
     return portfolios
 
-def efficient_frontier(portfolios, s=15, color = "k",alpha = 0.5, marker = 'o', figsize = (24,18), rf = 0.02):
+def efficient_frontier(portfolios, benchmark, s=15, color = "k",alpha = 0.5, marker = 'o', figsize = (24,18), rf = 2, start = start, end = end):
     portfolios.plot.scatter(x="Volatility",
                             y="Returns",
                             marker=marker,
@@ -67,7 +76,26 @@ def efficient_frontier(portfolios, s=15, color = "k",alpha = 0.5, marker = 'o', 
                             grid = True,
                             figsize = figsize)
     optimal_risky_port = portfolios.iloc[((portfolios["Returns"]-rf)/portfolios["Volatility"]).idxmax()]
-    plt.scatter(optimal_risky_port[1], optimal_risky_port[0], color='forestgreen', marker='*', s=500) 
+    optimal_treynor_port = portfolios.iloc[((portfolios["Returns"]-rf)/portfolios["Portfolio Beta"]).idxmax()]
+    plt.scatter(optimal_risky_port[1], 
+                optimal_risky_port[0], 
+                color='forestgreen', 
+                marker='*', 
+                s=500)
+    plt.scatter(optimal_treynor_port[1], 
+                optimal_treynor_port[0], 
+                color='orange', 
+                marker='*', 
+                s=500)
+    market = web.DataReader(benchmark, "yahoo", start = start, end = end)["Adj Close"]
+    market = pd.DataFrame(market)
+    market_mean = market.resample("Y").last().pct_change().mean()*100
+    optimal_jensens_alpha = portfolios.iloc[(portfolios["Jensen's Alpha"]).idxmax()]
+    plt.scatter(optimal_jensens_alpha[1], 
+                optimal_jensens_alpha[0], 
+                color = "purple", 
+                marker = "*",
+                s = 500)
     plt.title("Efficient Frontier")
     plt.xlabel("Risk")
     plt.ylabel("Expected Returns");
@@ -81,3 +109,8 @@ def optimal_treynor_ratio(portfolios, rf = 0.02):
     optimal_treynor_port = portfolios.iloc[((portfolios["Returns"]-rf)/portfolios["Portfolio Beta"]).idxmax()]
     optimal_treynor_port = pd.DataFrame(optimal_treynor_port).T
     return optimal_treynor_port
+
+def optimal_jensens_alpha(portfolios):
+    optimal = portfolios.iloc[(portfolios["Jensen's Alpha"]).idxmax()]
+    optimal = pd.DataFrame(optimal).T
+    return optimal
